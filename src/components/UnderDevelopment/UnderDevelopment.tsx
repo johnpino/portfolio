02094aabf29fe652efa215cfa6b2c2f02717ea4c
John Pino <iam@johnpino.me>
2023-06-11 00:51:49 -0500

    feat(components): improve UI for UnderDevelopment component

--- src/components/UnderDevelopment/UnderDevelopment.tsx
+++ src/components/UnderDevelopment/UnderDevelopment.tsx
@@ -1,5 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 
 export default function UnderDevelopment() {
-	return <div>Website is under development</div>
+	return (
+		<div className="text-center">
+			<h1 className="mb-2 text-4xl">
+				From the desk of <br />
+				<span className="text-6xl font-bold">John Pino</span>
+			</h1>
+			<h2 className="mb-4">Frontend Development</h2>
+			<p className="text-sm italic font-light">Under development</p>
+		</div>
+	)
 }
