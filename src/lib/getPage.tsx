780cbc0b22719a6c4fd2dab9e2e5d10644547970
John Pino <iam@johnpino.me>
2023-06-12 12:22:21 -0500

    feat: refactor getPage and getClient to better support preview queries

--- src/lib/getPage.tsx
+++ src/lib/getPage.tsx
@@ -3,10 +3,25 @@ import { gql } from '@apollo/client'
 
 import type { PageCollection, Page } from '@/types/Page'
 
-export default async function getPage(slug: string): Promise<Page> {
+import { draftMode } from 'next/headers'
+
+import assert from 'assert'
+
+assert(process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN)
+assert(process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN)
+
+export default async function getPage(slug: string, preview?: boolean): Promise<Page | null> {
+	const { isEnabled: isDraftMode } = draftMode()
+
+	const shouldUsePreview: boolean = preview || (isDraftMode && preview === undefined)
+
+	const authHeader = shouldUsePreview
+		? `Bearer ${process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN}`
+		: `Bearer ${process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN}`
+
 	const query = gql`
 		query pageCollectionQuery {
-			pageCollection(where: { slug: "${slug}" }) {
+			pageCollection(where: { slug: "${slug}" }, preview: ${shouldUsePreview}) {
 				items {
 					sys {
 						id
@@ -16,7 +31,11 @@ export default async function getPage(slug: string): Promise<Page> {
                     description
                     featuresCollection {
                         items{
+							__typename
                             ...on Disabled {
+								sys {
+									id
+								}
                                 heading1stRow
                                 heading2ndRow
                                 subheading
@@ -29,7 +48,18 @@ export default async function getPage(slug: string): Promise<Page> {
 		}
 	`
 
-	const { data }: { data: PageCollection } = await getClient().query({ query })
+	const { data }: { data: PageCollection } = await getClient().query({
+		query,
+		context: {
+			headers: {
+				authorization: authHeader,
+			},
+		},
+	})
+
+	if (data.pageCollection.items.length === 0) {
+		return null
+	}
 
 	return data.pageCollection.items[0]
 }
