245d26817903a1bace7e3f2495903a3a261b5772
John Pino <iam@johnpino.me>
2023-06-15 20:47:31 -0500

    test: add Disabled test

---
+++ src/components/Disabled/Disabled.mocks.ts
@@ -0,0 +1,14 @@
+import { Feature } from '@/types/Page'
+
+const DisabledMock: Feature = {
+	__typename: 'Disabled',
+	sys: {
+		id: '22dQ9kI0LmOzCrMgydJKw5',
+	},
+	heading1stRow: 'From the Desk of',
+	heading2ndRow: 'John Pino',
+	subheading: 'Frontend Development',
+	description: 'Under development',
+}
+
+export default DisabledMock
---
+++ src/components/Disabled/Disabled.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { render, screen } from '@testing-library/react'
+
+import Disabled from './Disabled'
+import DisabledMock from './Disabled.mocks'
+
+describe('Disabled', () => {
+	test('should render', () => {
+		render(<Disabled {...DisabledMock} />)
+
+		expect(screen.getByText('From the Desk of')).toBeInTheDocument()
+		expect(screen.getByText('John Pino')).toBeInTheDocument()
+		expect(screen.getByText('Frontend Development')).toBeInTheDocument()
+		expect(screen.getByText('Under development')).toBeInTheDocument()
+	})
+})
