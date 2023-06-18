634303fed7942f1d96b9f7314f9006a6c4248ce7
John Pino <iam@johnpino.me>
2023-06-18 11:25:10 -0500

    chore: adjust scripts and other improvements

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	webpack(config) {
+		// eslint-disable-next-line no-param-reassign
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'@/vars': path.resolve(__dirname, '.css-vars'),
+		}
+
+		return config
+	},
+}
 
 module.exports = nextConfig
